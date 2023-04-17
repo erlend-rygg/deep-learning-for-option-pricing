028b3f699f1cf5ba00e0e9677d395df782869162
Erlend <erlendrygg@gmail.com>
2023-04-17 12:33:35 +0200

    Heston model clean up, and replace multiprocessing with initial parameters continuation

--- Econometric models/Heston.py
+++ Econometric models/Heston.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pandas as pd
-from numba import jit, prange
+from numba import jit
 from lmfit import Parameters, minimize
 from scipy.integrate import quad
-import cProfile
-from multiprocessing import Pool, cpu_count
 import time
-from dateutil.relativedelta import relativedelta
-from datetime import datetime
 
 i = complex (0,1) # Define complex number i
+
+colab_path = "M:/Master/"
 
 @jit
 def charHeston(u, S0, r, T, sigma, kappa, theta, v0, rho):
@@ -62,7 +60,7 @@ def priceHestonIntegral(S0, K, r, T, sigma, kappa, theta, v0, rho, maxIntegral =
     return 0.5 * (S0 - K * np.exp(-r * T)) + integral/np.pi
 
 def iter_cb(params, iter, resid):
-    '''Callback function to print the parameters at each iteration of the minimizer'''
+    '''Callback function to print the parameters at each iteration of the minimizer for debuging'''
     parameters = [params['sigma'].value, 
                   params['kappa'].value, 
                   params['theta'].value, 
@@ -71,16 +69,16 @@ def iter_cb(params, iter, resid):
                   np.sum(resid)/len(resid)]
     print(parameters) 
 
-def calibrateHeston(optionPrices, S0, strikes, rates, maturities):
+def calibrateHeston(optionPrices, S0, strikes, rates, maturities, initial_parameters):
     '''Calibrate the Heston model parameters using the Levenberg Marquardt algorithm'''
 
     # Define the parameters to calibrate
     params = Parameters()
-    params.add('sigma',value = 0.028, min = 1e-2, max = 1)
-    params.add('kappa',value = 0.042, min = 1e-3, max = 5)
-    params.add('theta',value = 0.001, min = 1e-4, max = 0.1)
-    params.add('v0', value = 0.028, min = 1e-3, max = 0.5)
-    params.add('rho', value = -6e-7, min = -0.5, max = 0)
+    params.add('sigma',value = initial_parameters[0], min = 1e-2, max = 1)
+    params.add('kappa',value = initial_parameters[1], min = 1e-3, max = 5)
+    params.add('theta',value = initial_parameters[2], min = 1e-4, max = 0.1)
+    params.add('v0', value = initial_parameters[3], min = 1e-3, max = 0.5)
+    params.add('rho', value = initial_parameters[4], min = -0.5, max = 0)
 
     # Define the objective function to minimize as squared errors
     objectiveFunctionHeston = lambda paramVect: (optionPrices - priceHestonIntegral(S0, strikes,  
@@ -96,17 +94,17 @@ def calibrateHeston(optionPrices, S0, strikes, rates, maturities):
                       params, 
                       method = 'leastsq',
 #                      iter_cb = iter_cb,
-                      ftol = 1e-3) 
+                      ftol = 1e-4) 
     return(result)
 
 @jit(forceobj=True)
-def create_data_np_grouped(group):
+def create_data_np(df):
     '''Create numpy arrays with required data for calibration and testing'''
-    optionPrices = group['Price'].values
-    S0 = group['Underlying_last'].values[0]
-    strikes = group['Strike'].values
-    rates = group['R'].values
-    maturities = group['TTM'].values
+    optionPrices = df['Price'].values
+    S0 = df['Underlying_last'].values[0]
+    strikes = df['Strike'].values
+    rates = df['R'].values
+    maturities = df['TTM'].values
 
     data_np = np.empty((len(optionPrices), 5))
     data_np[:, 0] = optionPrices
@@ -117,16 +115,19 @@ def create_data_np_grouped(group):
 
     return data_np
 
-def calculateHestonDate(data_cal, data_test, test_date):
+def calculateHestonDate(data_cal, data_test, test_date, initial_parameters):
+    '''Calculate the heston model parameters option data for date t-1 and calculate the option price for date t'''
     # Calibrate the Heston model
     calibrationResult = calibrateHeston(
         data_cal[:, 0],
         data_cal[0, 1],
         data_cal[:, 2],
         data_cal[:, 3],
-        data_cal[:, 4]
+        data_cal[:, 4],
+        initial_parameters
     )
 
+    # Extract the parameters
     params = np.array([
         calibrationResult.params['sigma'].value,
         calibrationResult.params['kappa'].value,
@@ -146,73 +147,46 @@ def calculateHestonDate(data_cal, data_test, test_date):
     print(f'{(np.sum((optionPricesHeston - data_test[:, 0]) ** 2) / len(optionPricesHeston))**(0.5):.4f} RMSE for {test_date}')
     return optionPricesHeston, params
 
-def apply_moneyness_filter():
-    train_window = 11
-    validation_window = 1
-    test_window = 1
-    file = './data/processed_data/2013-2022_wo_lags.csv'
-    df = pd.read_csv(file)
-    df['TTM'] = df['TTM'] / 365
-    df['R'] = df['R'] / 100
-    df['Moneyness'] = df['Underlying_last'] / df['Strike']
-    print(f'Len before {len(df[df["Quote_date"] >= "2014-12-01"])}')
-
-    first_test_winow_start_date = datetime(2014,12,1)
-    last_test_winow_start_date = datetime(2022, 12, 1)
-
-    df_filtered = pd.DataFrame()
-
-    for i in prange((relativedelta(last_test_winow_start_date, first_test_winow_start_date)).months + (relativedelta(last_test_winow_start_date, first_test_winow_start_date)).years*12 + 1):
-        test_date = first_test_winow_start_date + relativedelta(months = i)
-        df_train = df[(df['Quote_date'] >= str(test_date - relativedelta(months = train_window) - relativedelta(months = validation_window))) & (df['Quote_date'] < str(test_date - relativedelta(months = validation_window)))]
-    
-        top = df_train['Moneyness'].quantile(0.95)
-        bottom = df_train['Moneyness'].quantile(0.05)
-
-        print(f'Filtering for {test_date} with {bottom} and {top}')
-
-        df_test = df[(df['Quote_date'] >= str(test_date)) & (df['Quote_date'] < str(test_date + relativedelta(months = test_window)))]
-
-        df_filtered = df_filtered.append(df_test[(df_test['Moneyness'] >= bottom) & (df_test['Moneyness'] <= top)]) 
-   
-    file = './data/processed_data/2015-2022_wo_lags_Heston_moneyness_filter.csv'
-    df_filtered.to_csv(file)
-    print(f'Len after {len(df_filtered)}')
-
 def HestonYear(df, year):
+    '''Calculate the Heston model parameters for each day of the year and calculate the option price for the next day'''
     # Extract last date of previous year and apply yearly filter
     first_date = df[df['Quote_date'] < f'{year}-01-01']['Quote_date'].max()
     df_year = df[(df['Quote_date'] >= first_date) & (df['Quote_date'] <= f'{year}-12-31')]
 
-    # Group the data by date and apply create_data_np_grouped to each group
-    dates = np.sort(df['Quote_date'].unique())
-    data_nps = df_year.groupby('Quote_date').apply(create_data_np_grouped)
-    data_nps = [(data_nps[i], data_nps[i+1], dates[i+1]) for i in range(len(data_nps)-1)]
+    # Group the data by date extract numpy arrays with data for each day
+    dates = np.sort(df_year['Quote_date'].unique())
+    data_nps = df_year.groupby('Quote_date').apply(create_data_np)
 
+    parameters = [0.02936084231134991, 0.040435318414851, 0.07357532840670486, 0.01697363646273281, -9.814464152491098e-08] # Initial parameters calculated on for last day of 2014
+    optionPrices_list = []
+    parameters_list = []
+    
     t = time.time()
-    print(f'Cpu count: {cpu_count()}')
-    with Pool(cpu_count()) as p:
-        results = p.starmap(calculateHestonDate, data_nps)    
-    print(f'{year} time: {time.time() - t}')  
-
-    results = np.array(results)
+    for i in range(len(data_nps-1)):
+        optionPrice, parameters = calculateHestonDate(data_nps[i], data_nps[i+1], dates[i+1], parameters)
+        optionPrices_list.append(optionPrice)
+        parameters_list.append(parameters)
+    print(f'{year} finished with time: {time.time() - t}')  
 
     # Save option prices
     df = df[df['Quote_date'] != dates[0]]
-    df['Heston_price'] = np.concatenate(results[:, 0])
+    df['Heston_price'] = optionPrices_list
     print('=====================')
     print(f'Total RMSE {year}: {(np.sum((df["Heston_price"] - df["Price"]) ** 2) / len(df["Price"]))**(0.5)}')
     df.to_csv(f'./data/results/{dates[1]}_{dates[-1]} Heston.csv', index=False)
 
     # Save parameters
-    params = results[:, 1].tolist()
-    df_params = pd.DataFrame(params, columns=['sigma', 'kappa', 'theta', 'v0', 'rho'])
+    df_params = pd.DataFrame(parameters_list, columns=['sigma', 'kappa', 'theta', 'v0', 'rho'])
     df_params['Quote_date'] = dates[1:]
     df_params.to_csv(f'./data/results/{dates[1]}_{dates[-1]} Heston parameters.csv', index=False)
 
 if __name__ == '__main__':
-    file = './data/processed_data/2015-2022_wo_lags_Heston_moneyness_filter.csv'
+    file = './data/processed_data/2014_2022_moneyness_and_TTM_filtered.csv'
     df = pd.read_csv(file)
+    df['TTM'] = df['TTM'] / 365
+    df['R'] = df['R'] / 100
+    df.info()
+    print(df.head(3))
     for year in range(2015, 2023):
         HestonYear(df, year)
 
